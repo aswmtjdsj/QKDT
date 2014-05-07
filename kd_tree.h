5aed38d860aca5ba33d02ff3f10defa62ec34238
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-05-07 01:13:28 -0400

    Update kd_tree.h

--- kd_tree.h
+++ kd_tree.h
@@ -1,18 +1,22 @@
 #ifndef KD_TREE_H
 #define KD_TREE_H
 
+#include "common.h"
+
 template <class T>
 class KD_Point {
     public:
         T x, y;
+		int index;
 
         KD_Point() {}
-        KD_Point(T a, T b): x(a), y(b) {}
+        KD_Point(T a, T b, int idx): x(a), y(b), index(idx) {}
         KD_Point(const KD_Point &p) { *this = p; }
         
         KD_Point & operator =(const KD_Point &p) { 
             x = p.x;
             y = p.y;
+			index = p.index;
             return *this; 
         }
 
@@ -59,21 +63,27 @@ class KD_Tree {
         void readPointsByInteractive();
         void readPointsFromCMD();
 
+		void randomPoints(const int &);
+
         void build();
         Node * buildTree(int, int, int, KD_Point<double> &, KD_Point<double> &);
         void find_median(const int &, const int &, const int &, const int &, bool (cmp)(KD_Point<double>, KD_Point<double>));
 
-        std::vector< KD_Point<double> > search(const KD_Point<double> &, const KD_Point<double> &) const;
-        void searchTree(const Node *, std::vector< KD_Point<double> > &, const KD_Point<double> &, const KD_Point<double> &) const;
-        void reportSubTree(const Node *, std::vector< KD_Point<double> > &) const;
+        std::vector< KD_Point<double> > search(const KD_Point<double> &, const KD_Point<double> &) ;
+        void searchTree(const Node *, std::vector< KD_Point<double> > &, const KD_Point<double> &, const KD_Point<double> &) ;
+        void reportSubTree(const Node *, std::vector< KD_Point<double> > &) ;
 
         void search_test();
+        bool search_compare(const int, std::vector<float> &);
         void traverse_test(const Node *);
 
         std::vector< KD_Point<double> > point_set;
         int number;
         Node * root;
         KD_Point<double> tl, br;// region, the tree contains
+
+		double build_time;
+		double query_time;
 };
 
 #endif
